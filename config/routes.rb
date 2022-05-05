dd1590d3f286038b1b7443dd3d2bf4ccb2650178
rypilu <rpiluski@gmail.com>
2022-05-05 06:41:58 -0400

    added home and about routes, controllers, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # rails knows this is pointed to app/controllers/pages_controller.rb
+  # after controller name the #pages is the name of the method.
+  # This is telling rails to expect a home method in the pages.controller.rb
+  # pages controller with home method/action
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 end
