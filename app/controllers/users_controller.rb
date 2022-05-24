ac6e9a1611700ee48aaed9fdfbd43ddf02f434f4
rypilu <rpiluski@gmail.com>
2022-05-24 06:22:26 -0400

    add admin user access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,8 +42,8 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
-    flash[:notice] = "Account and all associated articles successfully deleted and you've been logged out"
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to root_path
   end
 
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit your own account"
       redirect_to @user
     end
