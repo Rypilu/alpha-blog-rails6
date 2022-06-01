74bc365123c72ae07617be50b7e9153b434d821f
rypilu <rpiluski@gmail.com>
2022-06-01 07:37:38 -0400

    admin user requirement and test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,11 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def sign_in_as(user)
+    # can't user user.password because it will return the hash not the string.
+    # so instead we're just providing the string of the pw defined in setup of categories_controller_test
+    post login_path, params: { session: { email: user.email, password: "password" } }
+  end
+
+
 end
